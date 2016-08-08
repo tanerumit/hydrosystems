e8459dfd53d5631a975f58989a06f64d27220f80
tanerumit <tanerumit@gmail.com>
2016-08-07 21:59:21 -0400

    initial commit

---
+++ R/hydrology.R
@@ -0,0 +1,41 @@
+#' Potential evapotranspiration (PET)
+#'
+#' \code{PET_calculate()} returns monthly or daily PET values with the desired
+#' calculation approach based on the input arguments
+#' \url{https://en.wikipedia.org/wiki/Evapotranspiration}
+#'
+#' @param date a time-series date object
+#' @param tavg a vector of average temperature values (°C)
+#' @param tdif a vector of differences computed from maximum and minimum
+#' temperatures
+#' @param lat latitude information (enter negative values for southern hemisphere)
+#' @param method PET calculation method (currently only hargreaves formula is
+#' used)
+#'
+#' @return the output is a vector of PET values
+
+PET_calculate <- function(date, tavg, tdif, lat, method = "hargreaves") {
+
+  #Extract years & months from the date object
+  years_num <- length(unique(as.numeric(format(date,"%Y"))))
+  months    <- as.numeric(format(date,"%m"))
+
+  lookUp <- data.frame(m = 1:12,
+    days.m = c(31,28,31,30,31,30,31,31,30,31,30,31),
+    days.j = c(15,46,75,106,136,167,197,228,259,289,320,350))
+
+  DaysInMonth <- lookUp$days.m[months]
+  JulianDay <- lookUp$days.j[months]
+
+  if(method == "hargreaves") {
+
+    dr = (1+0.033*cos(2*pi/365*JulianDay))
+    phi = pi/180*BasinLat
+    delta = 0.409*sin((2*pi/365*JulianDay)-1.39)
+    ws = acos(-tan(phi)*tan(delta))
+    Rs = ((24*60/pi)*0.082*dr*(ws*sin(phi)*sin(delta) +
+        cos(phi)*cos(delta)*sin(ws)))*0.408*DaysInMonth
+    PET = 0.0023*Rs*(tavg + 17.8)*sqrt(tdif)
+
+  }
+}
---
+++ R/scenario_discovery.R
@@ -0,0 +1,57 @@
+#' Generate a grid output of prim_boxes
+#'
+#' \code{prim_grid()} returns a data table of prim_boxes with given features of
+#' mass, density, coverage, and weighted coverage
+#' @param input a matrix of input parameter values
+#' @param y is a vector of response values
+#' @param minimum risk threshold required for the prim boxes
+#' @param vdim is number of dimensions to explore in the input matrix
+
+prim_grid <- function(input, y, threshold = 0, vdim = 3) {
+
+  #All combinations of input variables
+  var_com <- t(combn(x = 1:ncol(input), vdim))
+  num_com <- nrow(var_com)
+
+  #Store results in a list
+  out <- as.list(rep(NA,num_com))
+
+  #Loop through each combination
+  for (i in seq_len(num_com)) {
+
+    var_names <- paste(colnames(input)[var_com[i,]], collapse = "-")
+    var_x <- input[,var_com[i,]]
+
+    model_prim <- prim.box(x=var_x[,c(1,2)], y=var_y,
+      threshold.type=1, peel.alpha = 0.05, paste.alpha = 0.01)
+
+    #Template to store data
+    num <- model_prim$num.hdr.class
+    df <- data_frame(vars = 0, Boxes = seq_len(num),
+      fun = 0, mass = 0, dens = 0, cov = 0, wcov = 0)
+
+    #Points in the box
+    points_box <- sapply(1:num, function(x) model_prim$y[[x]])
+    points_all <- y
+
+    #Points of interest in the box
+    cond_box <- sapply(1:num, function(x) points_box[[x]][points_box[[x]] > threshold])
+    cond_all <- points_all[points_all > threshold]
+
+    #Scenario features
+    df$vars <- var_names
+    df$fun <- model_prim$y.fun[1:num]
+    df$mass <- sapply(1:num, function(x) length(points_box[[x]])/length(points_all))
+    df$dens <- sapply(1:num, function(x) length(cond_box[[x]])/length(points_box[[x]]))
+    df$cov  <- sapply(1:num, function(x) length(cond_box[[x]])/length(cond_all))
+    df$wcov <- sapply(1:num, function(x) sum(cond_box[[x]])/sum(cond_all))
+
+    out[[i]] <- df
+  }
+
+  #Combine results
+  out %<>% bind_rows(out)
+
+  #return output
+  return(out)
+}
