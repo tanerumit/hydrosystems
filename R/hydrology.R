739f8c97b2c63735058dc372e7790c3fa280b717
tanerumit <tanerumit@gmail.com>
2017-07-12 10:32:45 -0400

    improve documentation

--- R/hydrology.R
+++ R/hydrology.R
@@ -185,4 +185,22 @@ abcdCalibrate <- function(..., Q.obs, metric = "KGE", na.rm = FALSE) {
 
 }
 
+#' Water Year function
+#'
+#' \url{http://www.civil.uwaterloo.ca/watflood/manual/02_03_2.htm}
+#'
+#' @param dates time-series of dates as a date object
+#' @param start_month the first month of the water year
+#' @return the output is the calculated water year
+#' @export
+GetwaterYear <- function(dates, start_month = 9) {
+    dates.posix = as.POSIXlt(dates)
+    offset = ifelse(dates.posix$mon >= start_month - 1, 1, 0)
+    return(dates.posix$year + 1900 + offset)
+}
+
+
+
+
+
 
